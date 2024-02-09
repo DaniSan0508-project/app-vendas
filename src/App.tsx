1079dda800f1bcd923310cc87bc520059edaf532
Daniel Santos <danielsantos@sysfar.com.br>
2024-02-09 00:32:32 -0300

    adicionado prettier, eslint e styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { SafeAreaView, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
+import styled from 'styled-components/native';
 
-function App() {
+const StyledContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+`;
+
+const StyledText = styled.Text`
+  font-size: 24px;
+  color: #333333;
+`;
+
+const App: React.FC = () => {
   return (
-    <SafeAreaView>
-        <View>
-            <Text >Teste</Text>
-        </View>
-    </SafeAreaView>
+    <StyledContainer>
+      <StyledText>Teste</StyledText>
+    </StyledContainer>
   );
-}
+};
 
 export default App;
