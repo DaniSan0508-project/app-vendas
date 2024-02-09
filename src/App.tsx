02732853912e9fefa4095401e4682c6947cdd5f8
Daniel Santos <danielsantos@sysfar.com.br>
2024-02-08 23:08:15 -0300

    modificando pasta do aquivo inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, Text, View } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+        <View>
+            <Text >Teste</Text>
+        </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
