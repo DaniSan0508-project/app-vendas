1a93167f196e5b83b99d940d4dec54f6ad2ae858
Daniel Santos <danielsantos@sysfar.com.br>
2024-02-09 22:50:32 -0300

    feat: implementado input customizado

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,8 +1,13 @@
 import { Text } from "react-native";
+import { ContainerLogin } from "../styles/login.style";
+import Input from "../../../shared/components/input/Input";
 
 const Login = () => {
     return (
-        <Text>Login</Text>
+        <ContainerLogin>
+            <Text>teste</Text>
+           <Input /> 
+        </ContainerLogin>
     )
 }
 
